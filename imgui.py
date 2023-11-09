d28b05c909950a6ec2ca83fd1763b2cd879485e7
Daniel Sun <sun.daniel@outlook.com>
2023-11-09 05:31:43 +0000

    add jmake file

---
+++ imgui.py
@@ -0,0 +1,31 @@
+import jmake
+
+jmake.setupenv()
+
+workspace = jmake.Workspace('imgui')
+
+# we need the header files to compile the glfw backend
+glfw = jmake.package('glfw', 'https://github.com/DanDanCool/glfw')
+imgui = jmake.Project('imgui', jmake.Target.STATIC_LIBRARY)
+
+files = jmake.glob('.', ['*.h', '*.cpp'])
+files.extend([
+    'backends/imgui_impl_glfw.h',
+    'backends/imgui_impl_glfw.cpp',
+    'backends/imgui_impl_opengl3.h',
+    'backends/imgui_impl_opengl3.cpp',
+    'backends/imgui_impl_opengl3_loader.h',
+    'backends/imgui_impl_dx11.h',
+    'backends/imgui_impl_dx11.cpp',
+    'backends/imgui_impl_dx12.h',
+    'backends/imgui_impl_dx12.cpp'
+    ])
+
+imgui.add(jmake.fullpath(files))
+imgui.include(jmake.fullpath(['.', 'lib/glfw/include']))
+
+imgui.export(includes=jmake.fullpath('.'))
+
+workspace.add(imgui)
+
+jmake.generate(workspace)
